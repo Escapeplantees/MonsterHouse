b01c9f625aeacfe701eb73dc1d0d523005b098bc
Steven <Q5152701@tees.ac.uk>
2020-12-04 20:23:05 +0000

    more steam testing

--- lobby-testing.bat
+++ lobby-testing.bat
@@ -1,2 +1,3 @@
 @echo off
-"G:\UE\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "%cd%\anos.uproject" -game -log windowed
+set UE4Location=C:\Program Files\Epic Games\UE_4.25
+"%UE4Location%\Engine\Binaries\Win64\UE4Editor.exe" "%cd%\anos.uproject" -game -log windowed
